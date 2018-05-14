81a10ac6b6b2a0d6d7d91f63b1eb5b5998676ad1
axl <voices117@gmail.com>
2018-05-14 07:21:24 -0300

    adding prototype

---
+++ src/prototype/ClientSocket.h
@@ -0,0 +1,22 @@
+/*
+ * Created by Federico Manuel Gomez Peter
+ * Date: 02/05/2018.
+ */
+
+#ifndef __ClientSocket_H__
+#define __ClientSocket_H__
+
+#include <string>
+
+#include "CommunicationSocket.h"
+
+/**
+ * Socket que tiene la capacidad de realizar una conexion con el servidor,
+ * partiendo del dato del host y el port a donde conectarse
+ */
+class ClientSocket : public CommunicationSocket {
+   public:
+    ClientSocket(const char *hostName, const char *port);
+};
+
+#endif  //__ClientSocket_H__
---
+++ src/prototype/stream.h
@@ -0,0 +1,81 @@
+#include <unistd.h>
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include "Exception.h"
+
+#define SLIZE_BUFFER_SIZE 4096
+
+namespace IO {
+
+struct GameStateMsg {
+    char num_worms;
+    float positions[20 * 2];
+};
+
+enum class PlayerInput {
+    move_right,
+    move_left,
+    stop_move,
+};
+
+enum class MsgID { WorldUpdate };
+
+template <typename Msg>
+class Stream {
+   public:
+    Stream() {}
+    ~Stream() {
+        this->close();
+    }
+
+    void push(const Msg &m) {
+        std::lock_guard<std::mutex> lock(this->mutex);
+        this->q.push(m);
+        this->not_empty.notify_all();
+    }
+
+    bool pop(Msg &m, bool block = true) {
+        std::unique_lock<std::mutex> lock(this->mutex);
+        while (this->q.empty() && !this->closed) {
+            if (!block) {
+                return false;
+            }
+            this->not_empty.wait(lock);
+        }
+
+        if (this->closed) {
+            throw Exception{"closed"};
+        }
+
+        m = this->q.front();
+        this->q.pop();
+        return true;
+    }
+
+    Stream &operator<<(const Msg &m) {
+        this->push(m);
+        return *this;
+    }
+
+    Stream &operator>>(Msg &m) {
+        this->pop(m);
+        return *this;
+    }
+
+    void close() {
+        if (this->closed) {
+            return;
+        }
+        this->closed = true;
+        this->not_empty.notify_all();
+    }
+
+   private:
+    std::queue<Msg> q;
+    std::mutex mutex;
+    std::condition_variable not_empty;
+    std::atomic<bool> closed{false};
+};
+}  // namespace IO
