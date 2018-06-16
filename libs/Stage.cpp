052f7704a0a2b59f528070043a7c877aa9f195e7
axl <voices117@gmail.com>
2018-06-16 01:19:11 -0300

    adding support for angles in girders

--- libs/Stage.cpp
+++ libs/Stage.cpp
@@ -12,17 +12,19 @@ Worms::Stage::Stage() {
     this->players.push_back(Worms::WormData{health, Math::Point<float>{-3.0f, 20.0f}});
     this->players.push_back(Worms::WormData{health, Math::Point<float>{-5.0f, 20.0f}});
 
-    this->girders.push_back(Girder{11, 1.42f, Math::Point<float>{50.0f, 10.0f}});
-    this->girders.push_back(Girder{31, 1.42f, Math::Point<float>{-50.0f, 7.0f}});
-    this->girders.push_back(Girder{20, 1.42f, Math::Point<float>{-30.0f, 10.0f}});
-    this->girders.push_back(Girder{10, 1.42f, Math::Point<float>{0.0f, 13.0f}});
-    this->girders.push_back(Girder{4, 1.42f, Math::Point<float>{-5.0f, 9.5f}});
-    this->girders.push_back(Girder{8, 1.42f, Math::Point<float>{-17.0f, 2.0f}});
-    this->girders.push_back(Girder{14, 1.42f, Math::Point<float>{-11.0f, 5.0f}});
-    this->girders.push_back(Girder{2.5f, 1.42f, Math::Point<float>{-3.0f, 2.0f}});
-    this->girders.push_back(Girder{7, 1.42f, Math::Point<float>{5.0f, 2.0f}});
-    this->girders.push_back(Girder{13.4f, 1.42f, Math::Point<float>{30.0f, 15.0f}});
-    this->girders.push_back(Girder{13.4f, 1.42f, Math::Point<float>{11.0f, 2.0f}});
+    this->girders.push_back(Girder{11, 1.42f, 0.0f, Math::Point<float>{50.0f, 10.0f}});
+    this->girders.push_back(Girder{31, 1.42f, 0.0f, Math::Point<float>{-50.0f, 7.0f}});
+    this->girders.push_back(Girder{20, 1.42f, 0.0f, Math::Point<float>{-30.0f, 10.0f}});
+    this->girders.push_back(Girder{10, 1.42f, 0.0f, Math::Point<float>{0.0f, 13.0f}});
+    this->girders.push_back(Girder{4, 1.42f, 0.0f, Math::Point<float>{-5.0f, 9.5f}});
+    this->girders.push_back(Girder{8, 1.42f, 0.0f, Math::Point<float>{-17.0f, 2.0f}});
+    this->girders.push_back(Girder{14, 1.42f, 0.0f, Math::Point<float>{-11.0f, 5.0f}});
+    this->girders.push_back(Girder{2.5f, 1.42f, 0.0f, Math::Point<float>{-3.0f, 2.0f}});
+    this->girders.push_back(Girder{7, 1.42f, 0.0f, Math::Point<float>{5.0f, 2.0f}});
+    this->girders.push_back(Girder{13.4f, 1.42f, 0.0f, Math::Point<float>{30.0f, 15.0f}});
+    this->girders.push_back(Girder{13.4f, 1.42f, 0.0f, Math::Point<float>{11.0f, 2.0f}});
+    this->girders.push_back(Girder{10.0f, 1.42f, 30.0f, Math::Point<float>{15.0f, 24.0f}});
+    this->girders.push_back(Girder{8.0f, 1.42f, -30.0f, Math::Point<float>{40.0f, 13.0f}});
 }
 
 const std::vector<Worms::WormData>& Worms::Stage::getWorms() const {
