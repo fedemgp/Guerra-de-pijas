4691c9269624cae2d58a8dbfbbf466e4ca5e97dc
axl <voices117@gmail.com>
2018-06-16 15:23:33 -0300

    adding helper script to run the server + 2 clients

---
+++ run.sh
@@ -0,0 +1,35 @@
+set -e
+
+function on_exit {
+    kill -SIGINT $server_pid
+    kill -9 $client1_pid
+    kill -9 $client2_pid
+}
+
+trap on_exit INT TERM ERR
+
+# launches the server in backgroung
+./target/server 8888 &
+server_pid=$!
+
+# waits for the server to start
+sleep 1
+
+# starts a client in background
+./target/client localhost 8888 &
+client1_pid=$!
+
+# starts the second client in background
+./target/client localhost 8888 &
+client2_pid=$!
+
+set +e
+
+# waits for both clients to close
+wait $client1_pid
+wait $client2_pid
+
+sleep 1
+
+# kills the server
+kill -SIGINT $server_pid
